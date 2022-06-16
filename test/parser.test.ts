7187210b52193cd65a0b3c3f2b56f433f2a96064
Yeonjin Shin <litehell@litehell.info>
2022-06-16 05:37:17 +0000

    Print parser result when no json file on test

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,21 +1,27 @@
 import NamumarkParser from "../src/parser";
-import fs from "fs/promises";
+import { promises as fs, existsSync } from "fs";
 import { join } from "path";
 
 jest.setTimeout(5000);
 
 async function testParser(testFileName: string) {
-  const wikitext = await fs.readFile(
-    join(process.cwd(), `test/wikitexts/${testFileName}.txt`),
-    { encoding: "utf8" }
+  const wikitextPath = join(
+    process.cwd(),
+    `test/wikitexts/${testFileName}.txt`
   );
+  const goodResultPath = join(
+    process.cwd(),
+    `test/parseResults/${testFileName}.json`
+  );
+  const wikitext = await fs.readFile(wikitextPath, { encoding: "utf8" });
   const parser = new NamumarkParser(wikitext);
   const result = parser.parse();
+  if (!existsSync(goodResultPath)) {
+    console.log(`Parse result of ${testFileName}\n` + JSON.stringify(result));
+    throw new Error("goodResult file not found!");
+  }
   const goodResult = JSON.parse(
-    await fs.readFile(
-      join(process.cwd(), `test/parseResults/${testFileName}.json`),
-      { encoding: "utf8" }
-    )
+    await fs.readFile(goodResultPath, { encoding: "utf8" })
   );
   expect(result).toStrictEqual(goodResult);
 }
